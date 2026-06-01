57dfab8878d76094517c9b3e86d485b4d4dc9655
Sebastian Salas <sebastiansalasrojas7@gmail.com>
2026-06-01 09:43:00 -0500

    chore(seeds): add candidate photo url seed for 2026 first round

---
+++ backend/src/main/resources/db/migration/V15__seed_candidates_photo_urls.sql
@@ -0,0 +1,40 @@
+﻿-- Seed candidate photos uploaded to Cloudinary
+-- Mapping rule: image 179..214 -> candidate id 1..36
+
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324416/EMS/candidates/1/photo.png' WHERE id = 1;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324418/EMS/candidates/2/photo.png' WHERE id = 2;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324420/EMS/candidates/3/photo.png' WHERE id = 3;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324423/EMS/candidates/4/photo.png' WHERE id = 4;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324424/EMS/candidates/5/photo.png' WHERE id = 5;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324427/EMS/candidates/6/photo.png' WHERE id = 6;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324429/EMS/candidates/7/photo.png' WHERE id = 7;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324430/EMS/candidates/8/photo.png' WHERE id = 8;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324433/EMS/candidates/9/photo.png' WHERE id = 9;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324434/EMS/candidates/10/photo.png' WHERE id = 10;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324437/EMS/candidates/11/photo.png' WHERE id = 11;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324439/EMS/candidates/12/photo.png' WHERE id = 12;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324441/EMS/candidates/13/photo.png' WHERE id = 13;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324443/EMS/candidates/14/photo.png' WHERE id = 14;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324445/EMS/candidates/15/photo.png' WHERE id = 15;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324447/EMS/candidates/16/photo.png' WHERE id = 16;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324449/EMS/candidates/17/photo.png' WHERE id = 17;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324451/EMS/candidates/18/photo.png' WHERE id = 18;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324453/EMS/candidates/19/photo.png' WHERE id = 19;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324456/EMS/candidates/20/photo.png' WHERE id = 20;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324458/EMS/candidates/21/photo.png' WHERE id = 21;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324460/EMS/candidates/22/photo.png' WHERE id = 22;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324462/EMS/candidates/23/photo.png' WHERE id = 23;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324464/EMS/candidates/24/photo.png' WHERE id = 24;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324466/EMS/candidates/25/photo.png' WHERE id = 25;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324468/EMS/candidates/26/photo.png' WHERE id = 26;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324470/EMS/candidates/27/photo.png' WHERE id = 27;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324472/EMS/candidates/28/photo.png' WHERE id = 28;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324474/EMS/candidates/29/photo.png' WHERE id = 29;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324477/EMS/candidates/30/photo.png' WHERE id = 30;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324479/EMS/candidates/31/photo.png' WHERE id = 31;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324481/EMS/candidates/32/photo.png' WHERE id = 32;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324483/EMS/candidates/33/photo.png' WHERE id = 33;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324485/EMS/candidates/34/photo.png' WHERE id = 34;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324488/EMS/candidates/35/photo.png' WHERE id = 35;
+UPDATE dbo.candidates SET photo_url = 'https://res.cloudinary.com/dvgangolz/image/upload/v1780324489/EMS/candidates/36/photo.png' WHERE id = 36;
+
