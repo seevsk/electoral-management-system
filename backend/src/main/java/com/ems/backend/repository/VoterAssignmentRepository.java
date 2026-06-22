3a6e37d44d93a16afc1d4a8d70b9e375d2937430
Sebastian Salas <sebastiansalasrojas7@gmail.com>
2026-06-22 16:53:20 -0500

    feat(voting): add lookup persistence models

---
+++ backend/src/main/java/com/ems/backend/repository/VoterAssignmentRepository.java
@@ -0,0 +1,22 @@
+package com.ems.backend.repository;
+
+import com.ems.backend.entity.VoterAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VoterAssignmentRepository extends JpaRepository<VoterAssignment, Integer> {
+
+    @Query("SELECT va FROM VoterAssignment va " +
+           "JOIN FETCH va.voter v " +
+           "JOIN FETCH v.account a " +
+           "JOIN FETCH va.votingTable vt " +
+           "JOIN FETCH vt.votingLocation vl " +
+           "JOIN FETCH vl.location l " +
+           "WHERE a.dni = :dni")
+    Optional<VoterAssignment> findWithDetailsByDni(@Param("dni") String dni);
+}
